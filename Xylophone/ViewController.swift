d5c42f0bd88725a0638cd898ad3788f7cf3eda4b
Алексей <dev@MacBook-Pro-Aleksej.local>
2023-01-26 11:36:35 +0300

    add Buttons, add setContraints()

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var stackButton: UIStackView!
+    @IBOutlet weak var cButton: UIButton!
+    @IBOutlet weak var dButton: UIButton!
+    @IBOutlet weak var eButton: UIButton!
+    @IBOutlet weak var fButton: UIButton!
+    @IBOutlet weak var gButton: UIButton!
+    @IBOutlet weak var aButton: UIButton!
+    @IBOutlet weak var bButton: UIButton!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setConstraints()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+
+    }
 }
 
+private extension ViewController {
+    func setConstraints(){
+        var padding: CGFloat = 10
+        [
+            cButton,
+            dButton,
+            eButton,
+            fButton,
+            gButton,
+            aButton,
+            bButton
+        ].forEach({ button in
+            guard let button = button else { return }
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.leadingAnchor.constraint(equalTo: stackButton.leadingAnchor, constant: padding).isActive = true
+            button.trailingAnchor.constraint(equalTo: stackButton.trailingAnchor, constant: -padding).isActive = true
+            padding += 5
+        })
+
+    }
+}
